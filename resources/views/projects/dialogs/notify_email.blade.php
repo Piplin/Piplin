1223e72871153a1bb2b4b71544b6f7936127fbb4
phecho <phecho@phecho.com>
2016-09-03 19:57:07 +0800

    Fixed trans

--- resources/views/projects/dialogs/notify_email.blade.php
+++ resources/views/projects/dialogs/notify_email.blade.php
@@ -3,7 +3,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
-                <h4 class="modal-title"><i class="ion ion-email"></i> <span>{{ trans('notifyemails.create') }}</span></h4>
+                <h4 class="modal-title"><i class="ion ion-email"></i> <span>{{ trans('notifyEmails.create') }}</span></h4>
             </div>
             <form role="form">
                 <input type="hidden" id="notifyemail_id" name="id" />
@@ -11,16 +11,16 @@
                 <div class="modal-body">
 
                     <div class="callout callout-danger">
-                        <i class="icon ion ion-alert"></i> {{ trans('notifyemails.warning') }}
+                        <i class="icon ion ion-alert"></i> {{ trans('notifyEmails.warning') }}
                     </div>
 
                     <div class="form-group">
-                        <label for="notifyemail_name">{{ trans('notifyemails.name') }}</label>
-                        <input type="text" class="form-control" id="notifyemail_name" name="name" placeholder="{{ trans('notifyemails.name') }}" />
+                        <label for="notifyemail_name">{{ trans('notifyEmails.name') }}</label>
+                        <input type="text" class="form-control" id="notifyemail_name" name="name" placeholder="{{ trans('notifyEmails.name') }}" />
                     </div>
                     <div class="form-group">
-                        <label for="notifyemail_address">{{ trans('notifyemails.email') }}</label>
-                        <input type="text" class="form-control" id="notifyemail_address" name="address" placeholder="{{ trans('notifyemails.address') }}" />
+                        <label for="notifyemail_address">{{ trans('notifyEmails.email') }}</label>
+                        <input type="text" class="form-control" id="notifyemail_address" name="address" placeholder="{{ trans('notifyEmails.address') }}" />
                     </div>
                 </div>
                 <div class="modal-footer">
