adcb410590eb416efa752cadde22b05cc83c9cbe
phecho <phecho@163.com>
2016-09-03 20:32:47 +0800

    Added a login test case

---
+++ tests/functional/LoginCept.php
@@ -0,0 +1,25 @@
+<?php 
+
+/*
+ * This file is part of Fixhub.
+ *
+ * Copyright (C) 2016 Fixhub.org
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+$I = new FunctionalTester($scenario);
+$I->wantTo('login as a user');
+$I->haveRecord('users', [
+    'name'       => 'JohnSmith',
+    'email'      => 'john@smith.com',
+    'password'   => bcrypt('password'),
+]);
+$I->amOnPage('/auth/login');
+$I->fillField('login', 'JohnSmith');
+$I->fillField('password', 'password');
+$I->click('button[type=submit]');
+$I->amOnPage('/');
+$I->seeAuthentication();
+$I->see('JohnSmith', '.user-menu');
