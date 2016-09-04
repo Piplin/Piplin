86a15bc63d47b778a08a3498c1502d1698210b7b
phecho <phecho@163.com>
2016-09-04 08:35:24 +0800

    Add test case

--- tests/unit/UserTest.php
+++ tests/unit/UserTest.php
@@ -11,20 +11,39 @@
 
 use Fixhub\Models\User;
 
-use Illuminate\Support\Facades\Hash;
-
 class UserTest extends \Codeception\TestCase\Test
 {
     /**
      * @var \UnitTester
      */
     protected $tester;
+
+    protected function _before()
+    {
+        //
+    }
+
+    protected function _after()
+    {
+        //
+    }
+
     public function testRegister()
     {
         $name = 'phecho';
         $email = 'phecho@fixhub.org';
         $password ='password';
+
         User::create(['name' => $name, 'email' => $email, 'password' => $password]);
+
         $this->tester->seeRecord('users', ['name' => $name, 'email' => $email]);
+    }
+
+    public function testIfModelReturnsAdminUserEmail()
+    {
+        // access model
+        $user = User::where('name', 'demo')->first();
+
+        $this->assertEquals('demo@fixhub.org', $user->email);
     }
 }
