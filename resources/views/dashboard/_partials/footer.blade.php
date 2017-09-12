138d4834d89c17e82309c5a03b43a1baf83ce04f
lianguan <guan.shiliang@gmail.com>
2017-09-12 14:52:04 +0800

    Update year of copyright

--- resources/views/dashboard/_partials/footer.blade.php
+++ resources/views/dashboard/_partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="container">
-        Copyright &copy; 2016 <strong><a href="http://fixhub.org">Fixhub</a></strong> Version {{ APP_VERSION }} All rights reserved.
+        Copyright &copy; 2016-{{ date('Y')}} <strong><a href="http://fixhub.org">Fixhub</a></strong> Version {{ APP_VERSION }} All rights reserved.
         <div class="pull-right hidden-xs">
           Inspired by Forge & Deployer
         </div>
