247b2d5ac6c1ae62990765bec42c7102f612bb20
lianguan <guan.shiliang@gmail.com>
2017-10-01 18:10:55 +0800

    Released 0.2.2

--- app/Console/Commands/InstallApp.php
+++ app/Console/Commands/InstallApp.php
@@ -344,8 +344,13 @@ class InstallApp extends Command
             return preg_replace('#/$#', '', $answer);
         };
 
-        $url = $this->askAndValidate('Application URL ("http://fixhub.app" for example)',
-                 [], $url_callback, 'http://fixhub.app');
+        $url = $this->askAndValidate(
+            'Application URL ("http://fixhub.app" for example)',
+            [],
+            $url_callback,
+            'http://fixhub.app'
+        );
+
         $region = $this->choice('Timezone region', array_keys($regions), 4);
 
         if ($region !== 'UTC') {
