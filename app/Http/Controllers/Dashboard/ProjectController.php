7c36a2bfe4cc5fd6ba2117994c5671b69aba0888
lianguan <guan.shiliang@gmail.com>
2017-10-07 21:00:41 +0800

    Loading data on demand

--- app/Http/Controllers/Dashboard/ProjectController.php
+++ app/Http/Controllers/Dashboard/ProjectController.php
@@ -38,22 +38,30 @@ class ProjectController extends Controller
             return $command->optional;
         });
 
-        return view('projects.show', [
+        $data = [
             'title'           => $project->group->name.'/'.$project->name,
             'project'         => $project,
-            'hooks'           => $project->hooks,
-            'sharedFiles'     => $project->sharedFiles,
-            'configFiles'     => $project->configFiles,
-            'variables'       => $project->variables,
-            'environments'    => $project->environments,
             'targetable_type' => 'Fixhub\\Models\\Project',
             'targetable_id'   => $project->id,
             'optional'        => $optional,
             'tags'            => $project->tags()->reverse(),
             'branches'        => $project->branches(),
-            'route'           => 'commands.step',
             'tab'             => $tab,
-        ]);
+        ];
+
+        $data['environments'] = $project->environments;
+        if ($tab == 'commands') {
+            $data['route'] = 'commands.step';
+            $data['variables'] = $project->variables;
+        } elseif ($tab == 'config-files') {
+            $data['configFiles'] = $project->configFiles;
+        } elseif ($tab == 'shared-files') {
+            $data['sharedFiles'] = $project->sharedFiles;
+        } elseif ($tab == 'hooks') {
+            $data['hooks'] = $project->hooks;
+        }
+
+        return view('projects.show', $data);
     }
 
     /**
