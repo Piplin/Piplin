3ef10560df2f9001234d050f9e60acfe4025b10f
lianguan <guan.shiliang@gmail.com>
2017-10-12 22:31:57 +0800

    Fix set_active helper

--- resources/views/admin/_partials/nav.blade.php
+++ resources/views/admin/_partials/nav.blade.php
@@ -1,9 +1,9 @@
 <div class="nav-tabs-custom">
     <ul class="nav nav-tabs">
         <li {!! set_active('admin') !!}><a href="/admin">{{ trans('admin.home') }}</a></li>
-        <li {!! set_active('admin/projects') !!}><a href="/admin/projects">{{ trans('projects.manage') }}</a></li>
-        <li {!! set_active('admin/users') !!}><a href="/admin/users">{{ trans('users.manage') }}</a></li>
-        <li {!! set_active('admin/links') !!}><a href="/admin/links">{{ trans('admin.misc') }}</a></li>
+        <li {!! set_active(['admin/projects', 'admin/groups', 'admin/templates*', 'admin/keys']) !!}><a href="/admin/projects">{{ trans('projects.manage') }}</a></li>
+        <li {!! set_active(['admin/users', 'admin/providers']) !!}><a href="/admin/users">{{ trans('users.manage') }}</a></li>
+        <li {!! set_active(['admin/links', 'admin/tips']) !!}><a href="/admin/links">{{ trans('admin.misc') }}</a></li>
         <li {!! set_active('admin/revisions') !!}><a href="/admin/revisions">{{ trans('revisions.manage') }}</a></li>
     </ul>
 </div>
