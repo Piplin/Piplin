e647f6af7676e02542b0e7fb320a5c09ef21721b
lianguan <guan.shiliang@gmail.com>
2017-10-18 19:34:47 +0800

    Clean up

--- resources/views/admin/groups/index.blade.php
+++ resources/views/admin/groups/index.blade.php
@@ -45,7 +45,7 @@
 
 @push('templates')
 <script type="text/template" id="group-template">
-    <td data-group-id="<%- id %>"><span class="drag-handle"><i class="ion ion-drag"></i></span><a href="/admin/groups/<%- id %>"><%- name %></a></td>
+    <td data-group-id="<%- id %>"><span class="drag-handle"><i class="ion ion-drag"></i></span><%- name %></td>
     <td><%- project_count %></td>
     <td>
         <div class="btn-group pull-right">
