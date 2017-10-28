c58e9a51ad52fdc6f017f0d08630e8bc6ae9466f
lianguan <guan.shiliang@gmail.com>
2017-10-28 17:58:52 +0800

    Change bg color of shortcut

--- resources/views/dashboard/_partials/shortcut.blade.php
+++ resources/views/dashboard/_partials/shortcut.blade.php
@@ -19,7 +19,7 @@
   <div class="col-md-3 col-sm-6 col-xs-12">
         <a href="{{ route('admin.groups.index') }}">
           <div class="info-box">
-            <span class="info-box-icon bg-green"><i class="fixhub fixhub-group"></i></span>
+            <span class="info-box-icon bg-blue"><i class="fixhub fixhub-group"></i></span>
 
             <div class="info-box-content">
               <span class="info-box-text">{{ trans('groups.manage') }}</span>
