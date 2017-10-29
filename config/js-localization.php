968022e31ca8b688643619f074464a719fa89e5f
lianguan <guan.shiliang@gmail.com>
2017-10-29 21:14:33 +0800

    Add missed translation

--- config/js-localization.php
+++ config/js-localization.php
@@ -41,7 +41,7 @@ return [
         'app'           => ['yes', 'no', 'never'],
         'dashboard'     => ['pending', 'pending_empty', 'running', 'running_empty', 'approving', 'approving_empty', 'deployment_number'],
         'deployments'   => ['create_success', 'completed', 'completed_with_errors', 'pending',
-                            'deploying', 'running', 'cancelled', 'failed', 'draft'],
+                            'deploying', 'running', 'cancelled', 'failed', 'draft', 'submit_success'],
         'variables'     => ['create', 'create_success', 'edit', 'edit_success', 'delete_success'],
         'environments'  => ['create', 'create_success', 'edit', 'edit_success', 'delete_success', 'link_auto', 'link_manual', 'link_success'],
         'projects'      => ['create', 'create_success', 'edit', 'edit_success', 'delete_success', 'finished', 'pending', 'deploying', 'failed', 'not_deployed'],
