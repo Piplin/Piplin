ac6586ed9fe65136eee8b30db5edb160da2d8f3f
lianguan <guan.shiliang@gmail.com>
2017-11-16 23:12:58 +0800

    Update

---
+++ resources/views/dashboard/plans/_partials/stages.blade.php
@@ -0,0 +1,27 @@
+<div class="box">
+    <div class="box-header">
+        <h3 class="box-title">{{ trans('commands.label') }} <i class="text-gray fixhub fixhub-help" data-toggle="tooltip" data-placement="right" data-original-title="{{ trans('commands.help') }}"></i></h3>
+    </div>
+    <div class="box-body table-responsive">
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>{{ trans('commands.stage') }}</th>
+                    <th>{{ trans('commands.before') }}</th>
+                    <th>{{ trans('commands.action') }}</th>
+                    <th>{{ trans('commands.after') }}</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach(['install', 'build', 'finish'] as $index => $stage)
+                <tr>
+                    <td>{{ $index+1 }}</td>
+                    <td>{{ $project->{'before_'.$stage} }}</td>
+                    <td><a href="{{ route('commands.step', ['id' => $project->id, 'command' => $stage]) }}">{{ trans('commands.'.$stage) }}</a> <i class="fixhub fixhub-info" data-toggle="tooltip" data-placement="right" data-html="true" data-original-title="{!! trans('commands.'.$stage.'_help') !!}"></i></td>
+                    <td>{{ $project->{'after_'.$stage} }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
--- resources/views/dashboard/plans/show.blade.php
+++ resources/views/dashboard/plans/show.blade.php
@@ -2,5 +2,20 @@
 
 @section('content')
     @include('dashboard.projects._partials.summary')
-    {{ $plan->name }} of {{ $plan->project->name }}
+    <div class="row">
+        <div class="col-md-12">
+            <div class="nav-tabs-custom">
+                <ul class="nav nav-tabs">
+                    <li><a href="">Builds</a></li>
+                    <li class="active"><a href="">Stages</a></li>
+                    <li><a href="">Artifact definitions</a></li>
+                </ul>
+                <div class="tab-content">
+                    <div class="tab-pane active">
+                        @include('dashboard.plans._partials.stages')
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
