916fe7f2d1e82b6469ef6514285b6ac173cef5e0
lianguan <guan.shiliang@gmail.com>
2017-11-23 14:41:01 +0800

    Fixed

--- database/migrations/2017_11_21_174746_rename_deployments_table.php
+++ database/migrations/2017_11_21_174746_rename_deployments_table.php
@@ -15,21 +15,7 @@ class RenameDeploymentsTable extends Migration
     {
         Schema::rename('deployments', 'tasks');
         Schema::rename('deployment_environment', 'environment_task');
-
-        Schema::table('environment_task', function (Blueprint $table) {
-            $table->renameColumn('deployment_id', 'task_id');
-        });
-        
-
         Schema::rename('deploy_steps', 'task_steps');
-
-        Schema::table('task_steps', function (Blueprint $table) {
-            $table->renameColumn('deployment_id', 'task_id');
-        });
-
-        Schema::table('server_logs', function (Blueprint $table) {
-            $table->renameColumn('deploy_step_id', 'task_step_id');
-        });
     }
 
     /**
@@ -41,19 +27,6 @@ class RenameDeploymentsTable extends Migration
     {
         Schema::rename('tasks', 'deployments');
         Schema::rename('environment_task', 'deployment_environment');
-
-        Schema::table('deployment_environment', function (Blueprint $table) {
-            $table->renameColumn('task_id', 'deployment_id');
-        });
-
         Schema::rename('task_steps', 'deploy_steps');
-
-        Schema::table('deploy_steps', function (Blueprint $table) {
-            $table->renameColumn('task_id', 'deployment_id');
-        });
-
-        Schema::table('server_logs', function (Blueprint $table) {
-            $table->renameColumn('task_step_id', 'deploy_step_id');
-        });
     }
 }
