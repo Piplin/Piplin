d523678af6d0fa95be7f1406ec1abe9f6d949604
lianguan <guan.shiliang@gmail.com>
2017-11-27 13:53:34 +0800

    Update

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -4,7 +4,8 @@
         <meta charset="UTF-8" />
         <title>{{ $app_name }} | Piplin - A continuous integration and delivery system</title>
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport" />
-
+        <link rel="icon" type="image/png" href="/img/favicon.ico">
+        <link rel="shortcut icon" href="/img/favicon.png" type="image/x-icon">
         <!-- Style -->
         <link href="{{ cdn('css/vendor.css') }}" rel="stylesheet" type="text/css" />
         <link href="{{ cdn('css/app.css') }}" rel="stylesheet" type="text/css" />
