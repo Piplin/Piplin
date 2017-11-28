e8234c9926cec7584e37ad026dfc4eb5035e7ada
Guan Shiliang <hifone@hifone.com>
2017-11-28 14:00:09 +0000

    Apply fixes from StyleCI

--- server.php
+++ server.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of Piplin.
  *
- * Copyright (C) 2016 Piplin.org
+ * Copyright (C) 2016-2017 piplin.com
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
