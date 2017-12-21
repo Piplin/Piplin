f84f9d1094c401e8913919fd712e24632de4e0b7
lianguan <guan.shiliang@gmail.com>
2017-12-21 11:25:49 +0800

    Integrate backup database manager

---
+++ app/Console/Commands/BackupDatabase.php
@@ -0,0 +1,70 @@
+<?php
+
+/*
+ * This file is part of Piplin.
+ *
+ * Copyright (C) 2016-2017 piplin.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Piplin\Console\Commands;
+
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Piplin\Services\Scripts\Runner as Process;
+
+/**
+ * Database backup manager.
+ */
+class BackupDatabase extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'piplin:backup-database';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Executes database backup for piplin';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->backupDatabase();
+    }
+
+    /**
+     * Backup the database.
+     */
+    protected function backupDatabase()
+    {
+        $date = Carbon::now()->format('Y-m-d_H-i-s');
+        $this->call('db:backup', [
+            '--database'        => config('database.default'),
+            '--destination'     => 'local',
+            '--destinationPath' => $date,
+            '--compression'     => 'gzip',
+        ]);
+    }
+}
---
+++ config/backup-manager.php
@@ -0,0 +1,61 @@
+<?php
+
+return [
+    'local' => [
+        'type' => 'Local',
+        'root' => database_path('backups'),
+    ],
+    's3' => [
+        'type' => 'AwsS3',
+        'key'    => '',
+        'secret' => '',
+        'region' => 'us-east-1',
+        'bucket' => '',
+        'root'   => '',
+    ],
+    'gcs' => [
+        'type' => 'Gcs',
+        'key'    => '',
+        'secret' => '',
+        'bucket' => '',
+        'root'   => '',
+    ],
+    'rackspace' => [
+        'type' => 'Rackspace',
+        'username' => '',
+        'key' => '',
+        'container' => '',
+        'zone' => '',
+        'endpoint' => 'https://identity.api.rackspacecloud.com/v2.0/',
+        'root' => '',
+    ],
+    'dropbox' => [
+        'type' => 'Dropbox',
+        'token' => '',
+        'key' => '',
+        'secret' => '',
+        'app' => '',
+        'root' => '',
+    ],
+    'ftp' => [
+        'type' => 'Ftp',
+        'host' => '',
+        'username' => '',
+        'password' => '',
+        'port' => 21,
+        'passive' => true,
+        'ssl' => true,
+        'timeout' => 30,
+        'root' => '',
+    ],
+    'sftp' => [
+        'type' => 'Sftp',
+        'host' => '',
+        'username' => '',
+        'password' => '',
+        'port' => 21,
+        'timeout' => 10,
+        'privateKey' => '',
+        'root' => '',
+    ],
+];
